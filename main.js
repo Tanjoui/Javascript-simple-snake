895ec94c6ba7390ae79f730ad1604d4662f47fb6
Tanjoui <32843605+Tanjoui@users.noreply.github.com>
2020-01-20 13:54:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,191 +1,182 @@
 
 class Snake {
 
-  getListe(){
-    return this.liste;
-  }
+	getListe(){
+		return this.liste;
+	}
 
-  deplacer(dir){};
+	constructor(){
+	    this.direction = 1; //direction par défaut = gauche
+	    this.liste = new Array(0); //liste des maillons
+	    //tete = liste[0]
+	    this.vivant = true; //vivant
+	    this.liste.unshift([5,5],[5,6]); //on ajoute 2 maillons au serpent
 
-  constructor(){
-    this.direction = 1; //direction par défaut = gauche
-    this.liste = new Array(0); //liste des maillons
-    //tete = liste[0]
-    this.vivant = true; //vivant
-    this.liste.unshift([5,5],[5,6]); //on ajoute 2 maillons au serpent
-
-    console.log(this.liste);
-  }
+	    console.log(this.liste);
+	}
 }
 
 
 class Model {
-  constructor(canvas) {
-    this.sizex = 10;
- 	this.sizey = 10;
-    this.grille = new Array(this.sizex).fill(new Array(this.sizey).fill(0));
-    this.snake = new Snake();
-    //création de la carte et d'un seprent
-    this.setSnake();
-    console.log(this.grille);
-  }
-  // 0 : vide
-  // 1 : head_snake
-  // 2 : body_snake
-  // 3 : fruit
-  setTile(x,y,val){
-    if (x < this.sizex && x > 0 && y < this.sizey && y > 0){
-      if ( val <= 3){
-        this.grille[x][y]=val;
-        console.log("Ecriture de "+ val + " aux coord "+x+":"+y);
-      }
+	constructor(canvas) {
+		this.sizex = 10;
+		this.sizey = 10;
+	    this.grille = new Array(this.sizex).fill(new Array(this.sizey).fill(0)); //map full zero
+	    this.snake = new Snake();
+	    //création de la carte et d'un seprent
+	    console.log(this.grille);
+	    addFruit(); //on place un fruit
+	    console.log(this.grille);
+	}
+	  // 0 : vide
+	  // 1 : head_snake
+	  // 2 : body_snake
+	  // 3 : fruit
+	setTile(x,y,val){
+	  	if (x < this.sizex && x > 0 && y < this.sizey && y > 0){
+	  		if ( val <= 3){
+	  			this.grille[x][y]=val;
+	  			console.log("Ecriture de "+ val + " aux coord "+x+":"+y);
+	  		}
+	  	}
+	}
+	getTile(x,y){
+	  	if (x < this.sizex && x > 0 && y < this.sizey && y > 0){
+	  		return grille[x][y];
+	  	}
+	  	else {
+	  		return 0;
+	  	}
+	}
+	getgrille(){
+	  	return grille;
+	}
+
+	addFruit(){
+	  	let x = grille.random();
+	  	let y = grille.random();
+	  	while (getTile(x,y) != 0){
+	  		x = grille.random();
+	  		y = grille.random();
+	  	}
+	  	setTile(x,y,3);
+	}
+
+
+
+
+
+  	step(){
+  		time = temps+1;
+        move(); //nouveau mouvement 
+        return matrice;
+  	}
+
+  	move(){//deplace le serpent
+        switch(snake.direction){ //génération d'une nouvelle tête selon la direction
+          case 1 :
+          head = (snake.liste[0]-1, snake.liste[1].y) 
+          case 2 :
+          head = (snake.liste[0], snake.liste[1].y-1)
+          case 3 :
+          head = (snake.liste[0]+1, snake.liste[1].y)
+          case 4 :
+          head = (snake.liste[0], snake.liste[1]+1)
+        }
+        checkWall(head[0], head[1])
+    	checkBody(head[0], head[1])
+        let found = checkFruit(head[0], head[1])// on passe les tests de collision
+
+        snake.liste.unshift(head); //on ajoute une nouvelle tête au debut
+        if(found = 0){ //si on a pas trouvé de fruit
+                snake.pop(); //on supprime le dernier element
+        }
     }
-  }
-  getTile(x,y){
-    if (x < this.sizex && x > 0 && y < this.sizey && y > 0){
-      return grille[x][y];
+
+    checkWall(x, y){
+      	if(x >= size || y>= size || x <= 0 || y < 0){
+      		console.log("PERDU")
+      		sound(2)
+      		Controller.reset()
+      	}
     }
-    else {
-      return 0;
+
+    checkBody(x, y){
+    	//vérifie si la nouvelle tête rencontre le body
+	    for(i=0; i<snake.liste.length; i++){
+	          	if(snake.liste[i][0] == x && snake.liste[i][1] == y){
+	          		console.log("PERDU")
+	          		sound(2)
+	          		Controller.reset()
+	          	}
+	        }
+	    }
+    checkFruit(x, y){
+        //vérifie si on mange un fruit
+        if(matrice[x][y] = 2){
+          	console.log("fruit trouvé")
+          	sound(1)
+          	return 1;
+        }else{
+          	return 0;
+        }
     }
-  }
-  getgrille(){
-    return grille;
-  }
 
-  addFruit(){
-    let x = grille.random();
-    let y = grille.random();
-    while (getTile(x,y) != 0){
-      x = grille.random();
-      y = grille.random();
-    }
-    setTile(x,y,3);
-  }
-  setSnake(){
-    let liste = this.snake.getListe();
-    for (var i = 0; i < liste.length; i ++) {
-      console.log("Contenu de e : "+ liste[i]);
-      this.setTile(liste[i][0],liste[i][1],1);
-    }
-  }
-  removeQueueSnake(){
-    setTile(snake.queue_coord_x,snake.queue_coord_y,1);
-  }
-}
+	turn(direction){// change la direction du serpent selon l'inpout
+		switch(direction){
+            case 1 ://gauche
+            if(head.dir == 3){
+            	console.log("move impossible")
+            	sound(2)
+            }else{
+            	snake.dir = 1
+            }
 
-          step(){
-                  time = temps+1;
-                  move(); //nouveau mouvement 
-                  return matrice;
-          }
+            case 2 ://haut
+            if(head.dir == 4){
+            	console.log("move impossible")
+            	sound(2)
+            }else{
+            	snake.dir = 2
+            }
 
-          move(){//deplace le serpent
-                  snake.head
-                  switch(snake.dir){ //génération d'une nouvelle tête selon la direction
-                          case 1 :
-                                  head = (snake.x-1, snake.y) 
-                          case 2 :
-                                  head = (snake.x, snake.y-1)
-                          case 3 :
-                                  head = (snake.x+1, snake.y)
-                          case 4 :
-                                  head = (snake.x, snake.y+1)
-                  }
-                  checkWall(head[0], head[1])
-                  checkBody(head[0], head[1])
-                  let found = checkFruit(head[0], head[1])// on passe les tests de collision
+            case 3 ://droite
+            if(head.dir == 1){
+            	console.log("move impossible")
+            	sound(2)
+            }else{
+            	snake.dir = 3
+            }
 
-                  snake.liste.unshift(head); //on ajoute une nouvelle tête au debut
-                  if(found = 0){ //si on a pas trouvé de fruit
-                          snake.pop(); //on supprime le dernier element
-                  }
-          }
-
-          checkWall(x, y){
-                  if(x >= size || y>= size || x <= 0 || y < 0){
-                          console.log("PERDU")
-                        sound(2)
-                        Controller.reset()
-                  }
-          }
-
-          checkBody(x, y){
-                  //vérifie si la nouvelle tête rencontre le body
-                  for(i=0; i<snake.liste.length; i++){
-                          if(snake.liste[i][0] == x && snake.liste[i][1] == y){
-                                  console.log("PERDU")
-                                sound(2)
-                                Controller.reset()
-                          }
-                  }
-          }
-          checkFruit(x, y){
-                  //vérifie si on mange un fruit
-                  if(matrice[x][y] = 2){
-                          console.log("fruit trouvé")
-                        sound(1)
-                        return 1;
-                  }else{
-                          return 0;
-                  }
-          }
-        
-        turn(direction){// change la direction du serpent selon l'inpout
-                switch(direction){
-                        case 1 ://gauche
-                                if(head.dir == 3){
-                                        console.log("move impossible")
-                                        sound(2)
-                                }else{
-                                        snake.dir = 1
-                                }
-                                
-                        case 2 ://haut
-                                if(head.dir == 4){
-                                        console.log("move impossible")
-                                        sound(2)
-                                }else{
-                                        snake.dir = 2
-                                }
-
-                        case 3 ://droite
-                                if(head.dir == 1){
-                                        console.log("move impossible")
-                                        sound(2)
-                                }else{
-                                        snake.dir = 3
-                                }
-
-                        case 4 : //bas
-                                if(head.dir == 2){
-                                        console.log("move impossible")
-                                        sound(2)
-                                }else{
-                                        snake.dir = 4
-                                }
-
-                }
+            case 4 : //bas
+            if(head.dir == 2){
+            	console.log("move impossible")
+            	sound(2)
+            }else{
+            	snake.dir = 4
+            }
 
         }
-        sound(id){// joue un son donné
-                var bruit = new Audio();
-                switch(id){
-                case 1:
-                        bruit.src = "son/yea.wav"
-                case 2:
-                        ruit.src = "son/blbl.wav"
-                case 3: 
-                        bruit.src = "son/gr.wav"
-                }
-                   bruit.play();
-        }
+    }
+
+	sound(id){// joue un son donné
+		var bruit = new Audio();
+		switch(id){
+			case 1:
+			bruit.src = "son/yea.wav"
+			case 2:
+			ruit.src = "son/blbl.wav"
+			case 3: 
+			bruit.src = "son/gr.wav"
+		}
+		bruit.play();
+	}
 
 
 }
 
 class View {
-  constructor() {}
+	constructor() {}
 
 
 }
@@ -193,41 +184,37 @@ class View {
 class Controller {
 
 
-  constructor(model, view) {
-    this.model = model
-    this.view = view
-  }
+	constructor(model, view) {
+		this.model = model
+		this.view = view
+	}
 
 
-function clavier(e){
+	function clavier(e){
+		k = e.keyCode;
+  		e.preventDefault(); //annuler le comportement par défaut des flèches 
+	  	switch(k) {
+	        case 37 : // touche gauche
+	        Model.turn(4)
+	        break;
+		    case 38 : // touche haut
+		    Model.turn(1)
+		    break;
+		    case 39 : // touche droite
+		    Model.turn(2)
+		    break;
+		    case 40 : // touche bas
+		    Model.turn(3)
+		    break;
+		    default :
+		}
+	}
 
-  k = e.keyCode;
-  e.preventDefault(); //annuler le comportement par défaut des flèches 
-
- switch(k) {
-           case 37 : // touche gauche
-      Model.turn(4)
-      break;
-    case 38 : // touche haut
-      Model.turn(1)
-      break;
-    case 39 : // touche droite
-      Model.turn(2)
-      break;
-    case 40 : // touche bas
-      Model.turn(3)
-      break;
-    default :
-  }
-
+	run(){
+	          setInterval(let matrice = Model.step() , 1000); //chaque seconde execute un step
+	}
 }
 
-  run(){
-          setInterval(let matrice = Model.step() , 1000); //chaque seconde execute un step
-
-  }
-}
-
-const app = new Controller(new Model(), new View())
-Controller.run();
+  const app = new Controller(new Model(), new View())
+  Controller.run();
 
